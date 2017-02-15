c37d979f0874222272365639adc888a42c126577
Isabel Ramos <iramos1226@gmail.com>
2017-02-14 19:49:55 -0600

    finished with main.js lesson

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log();
+// var outside = "global";
+
+// function scopeStuff() {
+// 	var inside = "local";
+// 	console.log("outside from inside the function:", outside); // global
+// 	console.log("inside from inside the function:", inside); //local
+// }
+
+// console.log("outside from outside the function:", outside); // global
+// console.log("inside from outside the function:", inside); // null
+
+// scopeStuff();
+
+// var global_base = 500;
+
+// function throwAway() {
+// 	global_base += 500;
+// 	console.log(global_base);
+// }
+
+// throwAway();
+
+var cats = (function() {
+	var color = "black";
+	var type = "tiger";
+
+	return {
+		getColor : function() {
+			return color;
+		},
+		getType : function() {
+			return type;
+		},
+		setColor : function(newColor) {
+			color = newColor;
+		}
+	};
+})();
+
+console.log(cats); // 
